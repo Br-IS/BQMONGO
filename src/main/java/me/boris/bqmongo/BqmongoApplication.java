a6c44e582652ac53c03f27b09d6dfe433ed8af9e
Br-IS <boris.quizhpe.est@tecazuay.edu.ec>
2022-09-02 23:58:00 -0500

    Agregacion de cors

--- src/main/java/me/boris/bqmongo/BqmongoApplication.java
+++ src/main/java/me/boris/bqmongo/BqmongoApplication.java
@@ -1,17 +1,26 @@
 package me.boris.bqmongo;
 
-import me.boris.bqmongo.repository.ProductoRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BqmongoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BqmongoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BqmongoApplication.class, args);
+    }
 
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("/**").allowedMethods("*").allowedOrigins("*");
+            }
+        };
+    }
 }
